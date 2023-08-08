89ad11b2765b2c76898481405a91dab327383eb6
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-08 19:32:00 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -12,3 +12,12 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-lock-s3-bucket-1"
+    key            = "terraform-statetf"   # Change this to a unique name for each configuration
+    region         = "ap-south-1"            # Change to your desired AWS region
+    
+  }
+}
+
