9284427ef61c60ba6c4d177d4203103c4cf5b6f6
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-08 11:18:42 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "terraform-state-lock-s3-bucket-2"
     key            = "terraform-statetf"   # Change this to a unique name for each configuration
     region         = "ap-south-1"            # Change to your desired AWS region
-    dynamodb_table = "dynamodb-lock-1"
+    
   }
 }
