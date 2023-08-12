b304731da4b507d69a031923b7b4c00820f7eafd
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-13 00:37:53 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-lock-s3-bucket-2"
+    bucket         = "terraform-state-lock-s3-bucket"
     key            = "main"                 # Change this to a unique name for each configuration
     region         = "ap-south-1"           # Change to your desired AWS region
     encrypt        = true                   # Set to true if you want to encrypt the state file
-    dynamodb_table = "dymanodb-lock-1"          # Optional: Use DynamoDB for locking
+    dynamodb_table = "dymanodb-lock"          # Optional: Use DynamoDB for locking
   }
 }
