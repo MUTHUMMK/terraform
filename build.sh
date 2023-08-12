ab0a197d55206a0b1e3117abe675d3209ebccfaa
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 15:47:41 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 docker build -t muthu:v1.1 .
 
 # login dockerhub & push the image 
-
+docker login -u $DOCKERHUBUSER -p $DOCKERHUBPSW
 docker tag muthu:v1.1 muthummkdh/new4
 docker push muthummkdh/new4
 
