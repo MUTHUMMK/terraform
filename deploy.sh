63183877499c1074622802075aab77d1ca1d7314
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 17:56:23 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -e
+
+# to get public IP and store the variable 
+a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIpAddress" --output text )
+
 echo $a
 
 # login the ssh-remote server & put the variable instead of public ip
