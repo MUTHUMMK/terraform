db0df9b1882bea1f305be24c52deb9f7e992defb
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 22:37:02 +0530

    create.sh

--- create.sh
+++ create.sh
@@ -19,4 +19,16 @@ echo "Terraform Exceute Successfully"
 
 
 
+# login the ssh-remote server & put the variable instead of public ip
+scp -o StrictHostKeyChecking=no -i  $sshkeyvar docker-compose.yml $ubuntu@$a:/home/ubuntu
+ssh -o StrictHostKeyChecking=no -i $sshkeyvar $ubuntu@$a<<EOF
 
+ls
+
+sudo apt-get update -y
+
+sudo apt-get install docker.io -y
+sudo apt-get install docker-compose -y
+
+sudo systemctl start docker
+EOF
