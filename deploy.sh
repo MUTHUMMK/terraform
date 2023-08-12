4f57c648da02b181f07d55271036b00fda87b902
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 17:12:56 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,16 @@ echo $a
 
 ssh -o StrictHostKeyChecking=no -i $sshkeyvar $ubuntu@$a<<EOF
 
+# login dockerhub
+docker login -u $DOCKERHUBUSER -p $DOCKERHUBPSW
+
 sudo docker-compose  up -d
 
 if curl localhost:80
 then
     echo "Successfully installed"
 fi
+
+docker logout
 
 EOF
