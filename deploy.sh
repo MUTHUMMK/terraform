d042da97b86caea0f7b99f8b2e0b93170b1ba362
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 13:20:21 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # to get public IP and store the variable 
-a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[ ].Instances[ ].PublicIp" --output text )
+a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIp" --output text )
 
 echo $a
 
