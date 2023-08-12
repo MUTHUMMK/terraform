dbb078a22cb5fe9be702d8b0ba2dbd41c0103187
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 08:49:19 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -10,8 +10,8 @@ terraform {
 
 resource provider "aws" {
   region = "ap-south-1"
-  access_key = "$ACCESSKEY"
-  secret_key = "$SECRETKEY"
+  access_key = "$AWS_ACCESS_KEY_ID "
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
 }
 
 
