cacdd3a824e88781a0a7b80151bfdba52748d10e
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 08:58:56 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,15 @@ resource "aws_subnet" "public-sub-1" {
     Name = "public-sub-1"
   }
 }
+resource "aws_subnet" "public-sub-2" {
+  vpc_id     = aws_vpc.vpc-id.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
 
+  tags = {
+    Name = "public-sub-2"
+  }
+}
 
 #INTERNET GATEWAY
 resource "aws_internet_gateway" "igw" {
@@ -41,12 +49,28 @@ resource "aws_route_table" "public-rt-1" {
     Name = "public-rt-1"
   }
 }
+resource "aws_route_table" "public-rt-2" {
+  vpc_id = aws_vpc.vpc-id.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+  tags = {
+    Name = "public-rt-2"
+  }
+}
 
 # rt associate
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public-sub-1.id
   route_table_id = aws_route_table.public-rt-1.id
+}
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public-sub-2.id
+  route_table_id = aws_route_table.public-rt-2.id
 }
 
 # SECURITY GROUP
@@ -86,4 +110,13 @@ resource "aws_instance" "public-1" {
   key_name                      = var.key
   associate_public_ip_address   = true
   tags                          = var.ec2-tags
+}
+resource "aws_instance" "public-2" {
+  ami                           = var.os
+  instance_type                 = var.size
+  subnet_id                     = aws_subnet.public-sub-2.id
+  vpc_security_group_ids        = [aws_security_group.all.id]
+  key_name                      = var.key 
+  associate_public_ip_address   = true
+  tags                          = var.ec2-tags1
 }
