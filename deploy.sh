fa0181a823e85637278cf430a13edd629653905f
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 17:31:00 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# to get public IP and store the variable 
+a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIpAddress" --output text )
+
 echo $a
 
 ssh -o StrictHostKeyChecking=no -i $sshkeyvar $ubuntu@$a<<EOF
