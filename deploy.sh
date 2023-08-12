2f12e008b76f5bd854aa98e021804a0dec6f2405
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 17:50:02 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# to get public IP and store the variable 
-a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIpAddress" --output text )
-
+set -e
 echo $a
 
+# login the ssh-remote server & put the variable instead of public ip
+scp -o StrictHostKeyChecking=no -i  $sshkeyvar docker-compose.yml $ubuntu@$a:/home/ubuntu
 ssh -o StrictHostKeyChecking=no -i $sshkeyvar $ubuntu@$a<<EOF
 
-# login dockerhub
-docker login -u $DOCKERHUBUSER -p $DOCKERHUBPSW
+ls
+
+sudo apt-get update -y
+
+sudo apt-get install docker.io -y
+sudo apt-get install docker-compose -y
 
 sudo docker-compose  up -d
 
@@ -17,6 +21,6 @@ then
     echo "Successfully installed"
 fi
 
-docker logout
+echo "deploy success"
 
 EOF
