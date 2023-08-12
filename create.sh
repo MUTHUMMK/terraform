bf725d49e570b54e878155166143adb991d439bd
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 13:03:43 +0530

    create.sh

--- create.sh
+++ create.sh
@@ -7,26 +7,12 @@ terraform init && terraform apply -auto-approve
 # to get public IP and store the variable 
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[ ].Instances[ ].PublicIp" --output text )
 
+b=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MUTHU" --query "Reservations[ ].Instances[ ].PublicIp" --output text )
+
 echo $a
+echo $b
 
-# login the ssh-remote server & put the variable instead of public ip
-scp -o StrictHostKeyChecking=no -i  $sshkeyvar docker-compose.yml ubuntu@$a:/home/ubuntu
-ssh -o StrictHostKeyChecking=no -i $sshkeyvar ubuntu@$a<<EOF
+echo "Terraform Exceute Successfully"
 
-ls
+sleep 5
 
-sudo apt-get update -y
-
-sudo apt-get install docker.io -y
-sudo apt-get install docker-compose -y
-
-sudo systemctl start docker
-
-sudo docker-compose  up -d
-
-if curl localhost:80
-then
-    echo "Successfully installed"
-fi
-
-EOF
