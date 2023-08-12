592ef202b8c9e61b19e15842bfb5ebad4a80652c
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 08:59:36 +0530

    output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
-output "publicIP" {
-    value = aws_instance.public-1.public_ip
+output "public_ip1" {
+  value = aws_instance.public-1.public_ip
+}
+output "public_ip2" {
+  value = aws_instance.public-2.public_ip
 }
