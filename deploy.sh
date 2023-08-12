9573f2eec9c7d160635a1a7d3c6dddd21be3e59d
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-12 13:54:17 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@
 # to get public IP and store the variable 
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIp" --output text )
 
-echo $a
+echo "$a"
+
+sleep 5
 
 # login the ssh-remote server & put the variable instead of public ip
-scp -o StrictHostKeyChecking=no -i  $sshkeyvar docker-compose.yml ubuntu@$a:/home/ubuntu
-ssh -o StrictHostKeyChecking=no -i $sshkeyvar ubuntu@$a<<EOF
+scp -o StrictHostKeyChecking=no -i "$sshkeyvar" docker-compose.yml ubuntu@"$a":/home/ubuntu
+ssh -o StrictHostKeyChecking=no -i "$sshkeyvar" ubuntu@"$a"<<EOF
 
 ls
 
