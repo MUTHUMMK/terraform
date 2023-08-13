3bb03e3572bde483c755d032a0ad18b4fa9db31c
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-13 15:26:41 +0530

    create.sh

--- create.sh
+++ create.sh
@@ -15,11 +15,10 @@ echo "$b"
 
 echo "Terraform Exceute Successfully"
 
-export SSH="$1"
 
 # login the ssh-remote server & put the variable instead of public ip
-scp -o StrictHostKeyChecking=no -i "$SSH" docker-compose.yml "$ubuntu"@"$a":/home/ubuntu
-ssh -o StrictHostKeyChecking=no -i "$SSH" "$ubuntu"@"$a"<<EOF
+scp -o StrictHostKeyChecking=no -i "$SSH_KEY" docker-compose.yml "$ubuntu"@"$a":/home/ubuntu
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" "$ubuntu"@"$a"<<EOF
 
 ls
 
