c2bdae83bf7ab20ba43d718033154e9a362e5da9
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-13 15:27:20 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@ set -e
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIpAddress" --output text )
 
 echo "$a"
-export SSH="$1"
 
-ssh -o StrictHostKeyChecking=no -i "$SSH" "$ubuntu"@"$a"<<EOF
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" "$ubuntu"@"$a"<<EOF
 
 sudo docker-compose  up -d
 
