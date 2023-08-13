b53605278109ed2ef9009884b60ac72148229e2a
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-13 15:39:23 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIpAddress" --output text )
 
 echo "$a"
