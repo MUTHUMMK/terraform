142570ff2b3e8790a1428aa5b0c4090691b0e853
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-13 11:14:15 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+set -e
+
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIpAddress" --output text )
 
-echo $a
+echo "$a"
 
-ssh -o StrictHostKeyChecking=no -i $sshkeyvar $ubuntu@$a<<EOF
+ssh -o StrictHostKeyChecking=no -i "$sshkeyvar" "$ubuntu"@"$a"<<EOF
 
 sudo docker-compose  up -d
 
-if curl localhost:80
-then
-    echo "Successfully installed"
-fi
+sleep 4
 
+echo "SUCCESSFULLY DEPLOYED"
 
 EOF
